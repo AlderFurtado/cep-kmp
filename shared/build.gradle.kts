637a85e51d130149cdb29532194ae74b09c8e19e
alderfurtado <aldermarcel12@gmail.com>
2025-06-18 08:53:03 -0300

    chore: adding publish maven central config

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,12 +1,13 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     kotlin("plugin.serialization") version "2.1.21"
     id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
 group = "com.cepkmp"
@@ -59,4 +60,48 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "com.cepkmp",
+        artifactId = "cepkmp",
+        version = "1.0.0"
+    )
+
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("CEP KMP")
+        description.set("Library used to get Address from postalcode (only works in Brazil)")
+        inceptionYear.set("2025")
+        url.set("https://github.com/AlderFurtado/cep-kmp")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("alder-furtado")
+                name.set("Alder Furtado")
+                url.set("https://github.com/AlderFurtado")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/AlderFurtado/cep-kmp")
+            connection = "scm:git:git://github.com/AlderFurtado/cep-kmp"
+            developerConnection = "scm:git:ssh://github.com/AlderFurtado/cep-kmp"
+        }
+    }
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
